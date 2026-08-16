3a6edd4c4cdb280b2ab80d96e28a7c2d65713ebf
unknown <delpyy.ea@gmail.com>
2026-08-16 11:08:53 +0700

    fix: production configs, force HTTPS, and exception logging for Vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,5 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->reportable(function (\Throwable $e) {
+            error_log('[BOOT EXCEPTION] ' . get_class($e) . ': ' . $e->getMessage() . "\n" . $e->getTraceAsString());
+        });
     })->create();
